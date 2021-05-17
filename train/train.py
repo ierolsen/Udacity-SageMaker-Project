f670e358cf9f2db11dc6168a70c4e4da909f1770
Erol <ierolsen@outlook.com>
2021-05-17 11:07:20 +0200

    add training loop

--- train/train.py
+++ train/train.py
@@ -68,8 +68,30 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     
     # TODO: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
 
-    pass
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            # forward: compute predicted outputs by passing inputs to the model
+            output = model(data)
+            # calculate the loss
+            loss = criterion(output, target)
+            # backward: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update running training loss
+            train_loss += loss.item()*data.size(0)
+            
+        print(f"Epoch: {epoch}, BCELoss: {total_loss / len(train_loader)}")
 
 
 if __name__ == '__main__':
